881d1d4ddb7a5b6f41e0c53744ecbfb4d2eff8ee
Work-PC <nbenza5@gmail.com>
2021-11-23 14:34:02 +0300

    added validation to contact form

--- src/components/contacts/ContactsForm/ContactsForm.js
+++ src/components/contacts/ContactsForm/ContactsForm.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Wrapper} from "../../../common/styles";
 import styled from "styled-components";
+import {useFormik} from "formik";
 
 const Form = styled.form`
   min-height: 300px;
@@ -42,13 +43,77 @@ const SubmitButton = styled.button`
   font-family: inherit;
 `
 
+const Error = styled.span`
+  margin: 5px 0;
+  color: red;
+  font-size: 14px;
+`
+
 const ContactsForm = () => {
+
+    const formik = useFormik({
+        initialValues: {
+            name: '',
+            email: '',
+            message: ''
+        },
+        validate: values => {
+            const errors = {};
+
+            if (!values.email) {
+                errors.email = 'Required';
+            } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+                errors.email = 'Invalid email address';
+            }
+
+            if (!values.name) {
+                errors.name = 'Required';
+            } else if (!/^[A-Z0-9._%+-]{3,}$/i.test(values.name)) {
+                errors.name = 'Name must be longer than 2 symbols';
+            }
+
+            if (!values.message) {
+                errors.message = 'Type some message';
+            }
+
+            return errors;
+
+        },
+        onSubmit: () => {
+            formik.resetForm()
+            formik.errors = {}
+        }
+    })
+
+
     return (
         <Wrapper justifyContent={'center'}>
-            <Form>
-                <Input placeholder={'Name'}/>
-                <Input placeholder={'Email'}/>
-                <Textarea type={'textarea'} placeholder={'Message'}/>
+            <Form onSubmit={formik.handleSubmit}>
+                <Input
+                    placeholder={'Name'}
+                    {...formik.getFieldProps('name')}
+                    onBlur={formik.handleBlur}
+                />
+                {formik.touched.name && formik.errors.name &&
+                <Error>{formik.errors.name}</Error>
+                }
+                <Input
+                    placeholder={'Email'}
+                    {...formik.getFieldProps('email')}
+                    onBlur={formik.handleBlur}
+                />
+                {formik.touched.email && formik.errors.email &&
+                <Error>{formik.errors.email}</Error>
+                }
+                <Textarea
+                    type={'textarea'}
+                    placeholder={'Message'}
+                    {...formik.getFieldProps('message')}
+                    onBlur={formik.handleBlur}
+                />
+                {formik.touched.message && formik.errors.message &&
+                <Error>{formik.errors.message}</Error>
+                }
                 <SubmitButton type={'submit'}>Send</SubmitButton>
             </Form>
         </Wrapper>
